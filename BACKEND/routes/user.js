5bb4c9078368618eb922aabefc020c93e48b05fd
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-03-08 15:39:38 +0530

    basic structure of routes/user.js

---
+++ BACKEND/routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const jwt = require("jsonwebtoken");
+
+const { User } = require("./db")
+const router = express.Router();
+
+router.post('/signup', (res, res) => {
+
+})
+
+router.post("/signin", (req, res) => {
+
+})
+
+module.exports = router;
