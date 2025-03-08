b4ed83cced9fbe4c01b19c1f1cc24500fd365426
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-03-08 13:54:59 +0530

    Initial commit

---
+++ BACKEND/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const { conectToDatabase } = require("./db");
+require("dontenv").config();
+
+//middleware
+app.use(express.json());
+
+//routes 
+app.use("/todos",todRouter);
+app.use("/user", userRouter);
+
+conectToDatabase().then(() => {
+    const PORT = process.env.PORT || 3000
+
+    app.listen(PORT, () => console.log('server is running on port ${PORT}'))
+})
